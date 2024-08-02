168bf064ca430e4f386946062a4845735d6e1402
MariaRuamah <151635380+MariaRuamah@users.noreply.github.com>
2024-08-02 09:26:29 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+const caixaprincipal = document.querySelector('.caixa-principal');
+const caixapergunta = document.querySelector('.caixa-pergunta');
+const caixaalternativa = document.querySelector('.caixa-alternativa');
+const caixaresultado = document.querySelector('.caixa-resultado');
+
+const perguntas = [//abre a lista da objetos (itens)
+    {//abre o item
+        enunciado:"pergunta 1",
+        alternativas:[
+            "alternativa 1",
+            "alternativa 2"
+        ]
+        
+    }
+    
+]
