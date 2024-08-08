6814a17c9c6d52e8f015a876c1444bd5b6e030d0
MariaRuamah <151635380+MariaRuamah@users.noreply.github.com>
2024-08-08 07:44:22 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -5,12 +5,25 @@ const caixaresultado = document.querySelector('.caixa-resultado');
 
 const perguntas = [//abre a lista da objetos (itens)
     {//abre o item
-        enunciado:"pergunta 1",
-        alternativas:[
-            "alternativa 1",
-            "alternativa 2"
-        ]
-        
-    }
-    
+        enunciado: "pergunta 1",
+        alternativas: ["alternativa 1", "alternativa 2"]
+    },
+    {
+        enunciado: "pergunta 2",
+        alternativas: ["alternativa 1", "alternativa 2"]
+    },
+}
+    enunciado: "pergunta 2",
+    alternativas: ["alternativa 1", "alternativa 2"]
+},
+{   
+    enunciado: "pergunta 3",
+    alternativas: ["alternativa 1", "alternativa 2"]
+}
 ]
+let posicao = 0;
+let perguntaatual;
+
+function mostrapergunta(){
+    
+}
